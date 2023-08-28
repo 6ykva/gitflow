ceac635f74ccecfc24184aece0d3461121f5d3ff
alexandr <6ykva.izh@gmail.com>
2023-08-28 23:30:28 +0400

    add def 'title_up' in str_func

--- str_func.py
+++ str_func.py
@@ -1,3 +1,11 @@
 def up(user_input):
     a = ""
     return a.upper()
+
+def title_up(up):
+    """
+    Вывод слов в строке с заглавной буквы
+    :param up:
+    :return:
+    """
+    return title_up(up)
